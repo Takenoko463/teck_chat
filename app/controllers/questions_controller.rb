107d7127de64922fa6f749beaaa4c8be2b8f26f5
Takenoko463 <83239026+Takenoko463@users.noreply.github.com>
2024-03-29 12:41:38 +0900

    indexアクション

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,2 +1,5 @@
 class QuestionsController < ApplicationController
+  def index # indexアクションを定義した
+    @questions = Question.all # すべてのレコードを@questionsに代入
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  #ルートpassを受け取った際に、postsコントローラーのindexアクションを使用する
+  root to: "questions#index"
 end
