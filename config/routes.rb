0364441781ff0d190528fee0af5e774e3fbc4b04
Takenoko463 <83239026+Takenoko463@users.noreply.github.com>
2024-03-29 12:52:50 +0900

    ルーティング調整

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   #ルートpassを受け取った際に、postsコントローラーのindexアクションを使用する
   root to: "questions#index"
+  #questions/newを受け取った際にnewコントローラーのnewアクションを使用する
+  get "questions/new", to: "questions#new"
 end
