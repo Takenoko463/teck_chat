5a31f61ebe6fdff3a66a7155228f74b3803a5e27
Takenoko463 <83239026+Takenoko463@users.noreply.github.com>
2024-04-04 11:40:08 +0900

    showコントローラーにindex変数を定義

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -13,7 +13,7 @@ class QuestionsController < ApplicationController
   end
 
   def show
-    @question = Question.find(id: 1)
+    @question = Question.find(params[:id])
     @answers = @question.answers
   end
 
