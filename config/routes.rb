c1a0f9d405d48ded377b138f042b708811a5aa5a
Takenoko463 <83239026+Takenoko463@users.noreply.github.com>
2024-04-04 11:31:55 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   #questionsというurlを受け取った際のアクションとして、index,new,create,showのいずれかアクションを許可
   resources :questions, only: [:index, :new, :create]
   #questionの詳細表示から、answerコントローラーを操作可能
-  resources :questions,only: :show do
-    resources :answers,only: [:index, :new, :create]
+  resources :questions, only: :show do
+    resources :answers, only: [:index, :new, :create]
   end
 end
